abb90a307c035071bb39b9361ec984bd9cdd08a5
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-02 23:10:04 -1000

    version 2 working postRequest code htmlString

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var http = require ('http');
 var fs = require ('fs');
-var queryString = ('queryString');
+var querystring = ('querystring');
 
 var server = http.createServer(function elementsServer (req, res){
   req.setEncoding('utf8');
+
+  var bodyData = {};
   req.on('data', function (data){
 
+    //Deserialize a query string to an object
+    bodyData = querystring.parse(data);
+    console.log(data);
 
   });
 
@@ -21,8 +26,6 @@ var server = http.createServer(function elementsServer (req, res){
       case 'POST':
 
       break;
-
-
     }
   });
 
@@ -45,7 +48,51 @@ var server = http.createServer(function elementsServer (req, res){
             }
         });
       }
-    }
+  }
+
+  function postRequests (filename){
+
+    var elementName;
+    var elementSymbol;
+    var elementAtomicNumber;
+    var elementDescription;
+
+    var path = 'public/' + filename + '.html';
+    var htmlString =
+
+    '<!DOCTYPE html>' +
+    '<html lang="en">' +
+    '<head>' +
+      '<meta charset="UTF-8">' +
+      '<title>The Elements - ' + elementName +'</title>' +
+      '<link rel="stylesheet" href="css/styles.css">' +
+    '</head>' +
+    '<body>' +
+      '<h1>' + elementName + '</h1>' +
+      '<h2>' + elementSymbol + '</h2>'+
+      '<h3>' + elementAtomicNumber + '</h3>' +
+      '<p>' + elementDescription + '</p>' +
+      '<p><a href="/">back</a></p>' +
+    '</body>' +
+    '</html>';
+
+    fs.readDir(path, function (err, data) {
+      //good habit to console status of err
+      if (err){
+        console.log(err);
+        res.statusCode = 500;
+        res.statusMessage = "Something went very wrong";
+        //use return to be explicit
+        return fs.readFile('public.404.html', function (err, _404){
+           return res.end(_404);
+        });
+      } else {
+        if (data.indexOf(path) === -1){
+
+        }
+      }
+    });
+  }
 
 
 });
